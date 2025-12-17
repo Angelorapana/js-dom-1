b2217b94a15c0adca5cd3cc0087f324c14d141d6
angelorapana <angelorapana@icloud.com>
2025-12-17 15:58:37 +0100

    Dichiarazione e recupero nel js gli elementi del dom attraverso getElementById

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const lampadina = document.getElementById("lampadina");
+const bottone = document.getElementById("attivaBottone");
+
+document.body.getElementById("attivaBottone").addEventListener("click", function() {
+   
